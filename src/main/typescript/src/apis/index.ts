784f86215d4c7655849f64931de178d76bca0580
Seifallah Guedouari <guedouari@gmail.com>
2025-06-03 17:43:36 +0100

    initial typescript genertion

---
+++ src/main/typescript/src/apis/index.ts
@@ -0,0 +1,4 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './HealthApi';
+export * from './InfoApi';
---
+++ src/main/typescript/src/models/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ErrorResponse';
+export * from './HealthResponse';
+export * from './VersionResponse';
