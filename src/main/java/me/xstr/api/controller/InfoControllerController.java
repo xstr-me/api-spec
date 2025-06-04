f22721217b6750329ac74d96a5d5b1267e2bf230
Seifallah Guedouari <guedouari@gmail.com>
2025-06-04 21:18:36 +0100

    remove generations from maven

---
+++ src/main/java/me/xstr/api/controller/InfoControllerController.java
@@ -0,0 +1,44 @@
+package me.xstr.api.controller;
+
+import me.xstr.api.model.VersionResponse;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.constraints.*;
+import jakarta.validation.Valid;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import jakarta.annotation.Generated;
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", comments = "Generator version: 7.13.0")
+@Controller
+@RequestMapping("${openapi.xStrMe.base-path:/v1}")
+public class InfoControllerController implements InfoController {
+
+    private final InfoControllerDelegate delegate;
+
+    public InfoControllerController(@Autowired(required = false) InfoControllerDelegate delegate) {
+        this.delegate = Optional.ofNullable(delegate).orElse(new InfoControllerDelegate() {});
+    }
+
+    @Override
+    public InfoControllerDelegate getDelegate() {
+        return delegate;
+    }
+
+}
